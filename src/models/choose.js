f925a12d3655b8ea0b48c19fed2990961c4872ec
gytg <2697266324@qq.com>
2017-11-17 19:43:56 +0800

    limit

--- src/models/choose.js
+++ src/models/choose.js
@@ -64,6 +64,7 @@ export default modelExtend(commonModel, {
             : data.results.phone
             , { expires: 1, path: '/' });
         cookie.set("remember",1, { expires: 1, path: '/' });
+        cookie.set("limit",10, { expires: 1, path: '/' });
         location.href = '/hoteladmin/#/panel_home'
       }
     }
